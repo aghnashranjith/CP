0443cd41a2ca83514e36bd9af124e40228f2461d
Aghnash Ranjith <aghnashranjith@gmail.com>
2022-11-09 09:28:43 +0530

    overthinking will get you killeddd bruhhh

--- LeetCode/Two Sum.cpp
+++ LeetCode/Two Sum.cpp
@@ -1,7 +1,12 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int p1=0,p2=1;
-        int hash[10000]
+        for(int i=0; i<nums.size(); i++)
+        {
+            for(int j=0; j<nums.size(); j++)
+                if(nums[i]+nums[j] == target && i!=j)
+                    return {i,j};
+        }
+        return {-1, -1};
     }
 };
