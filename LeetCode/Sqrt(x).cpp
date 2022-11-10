00e2c015b593ab51b02858ddeb863191a8a63265
Aghnash Ranjith <aghnashranjith@gmail.com>
2022-11-10 09:30:10 +0530

    easy

---
+++ LeetCode/Sqrt(x).cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int ans = 0;
+        for(int i=1; i<=x/i;i++)
+        {
+            if(i*i <= x)
+                ans = i;
+        }
+        return ans;
+    }
+};
